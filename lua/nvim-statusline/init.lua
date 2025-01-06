57078bb55fd32529be2d25c95abd2faa777c8c89
Mario Schillermann <mario.schillermann@gmail.com>
2025-01-06 07:54:30 +0100

    adjust for netrw explorer

--- lua/nvim-statusline/init.lua
+++ lua/nvim-statusline/init.lua
@@ -1,41 +1,62 @@
 local Module = {}
 
-vim.api.nvim_command("highlight NvimStatuslineErrors guibg=#6f1313")
-vim.api.nvim_command("highlight NvimStatuslineWarnings guibg=#264f78")
+vim.api.nvim_command("highlight NvimStatuslineErrors guifg=#e12636")
+vim.api.nvim_command("highlight NvimStatuslineWarnings guifg=#ffdf00")
+vim.api.nvim_command("highlight NvimStatuslineHints guifg=#ffffff")
+vim.api.nvim_command("highlight NvimStatuslineInfo guifg=#ffffff")
 
-function Module.statusline_explorer()
-  local git_branch = vim.fn.system("git branch --show-current 2> /dev/null | tr -d '\n'")
-  if (git_branch == "") then
-    return "  [No Branch]"
-  end
-  return string.format("  %s", git_branch) 
+function Module.get_git_branch_name()
+    local handle = io.popen('git rev-parse --abbrev-ref HEAD 2>/dev/null')
+    local branch = handle:read("*a")
+    handle:close()
+    return string.len(branch) > 0 and branch:gsub("\n", "") or ""
 end
 
-function Module.statusline_editor()
-  local errors = #vim.diagnostic.get(0, { severity = vim.diagnostic.severity.ERROR }) + #vim.diagnostic.get(0, { severity = vim.diagnostic.severity.WARN })
-  local info = #vim.diagnostic.get(0, { severity = vim.diagnostic.severity.INFO }) + #vim.diagnostic.get(0, { severity = vim.diagnostic.severity.HINTS })
-
-  local statusline = "  %t %=󰉻 %l/%L "
-  if (errors > 0) then
-    statusline = statusline .. "%#NvimStatuslineErrors#  " .. errors .. " %*"
-  else
-    statusline = statusline .. "  0 "
-  end
-
-  if (info > 0) then
-    statusline = statusline .. "%#NvimStatuslineWarnings#  " .. info .. " %*"
-  else
-    statusline = statusline .. "  0 "
-  end
-  return statusline
+function Module.lspNumberOfErrors()
+  return #vim.diagnostic.get(0, { severity = vim.diagnostic.severity.ERROR })
 end
 
-function Module.set_statusline()
-  if vim.bo.filetype == "NvimTree" then
-    vim.opt_local.statusline = Module.statusline_explorer()
-    return
-  end
-  vim.opt_local.statusline = Module.statusline_editor()
+function Module.lspNumberOfWarnings()
+  return #vim.diagnostic.get(0, { severity = vim.diagnostic.severity.WARN })
+end
+
+function Module.lspNumberOfHints()
+  return #vim.diagnostic.get(0, { severity = vim.diagnostic.severity.HINTS })
+end
+
+function Module.lspNumberOfInfo()
+  return #vim.diagnostic.get(0, { severity = vim.diagnostic.severity.INFO })
+end
+
+function Module.get_advanced_line()
+  return table.concat({
+    (vim.bo.readonly or not vim.bo.modifiable) and " " or "",
+    vim.bo.modified and "● " or "",
+    vim.bo.filetype == "help" and "' " or " ",
+    "%f",
+    Module.lspNumberOfErrors() > 0 and "%#NvimStatuslineErrors# %* " .. Module.lspNumberOfErrors() or "",
+    Module.lspNumberOfWarnings() > 0 and "%#NvimStatuslineWarnings# %* " .. Module.lspNumberOfWarnings() or "",
+    Module.lspNumberOfHints() > 0 and "%#NvimStatuslineHints# %* " .. Module.lspNumberOfHints() or "",
+    Module.lspNumberOfInfo() > 0 and "%#NvimStatuslineInfo# %* " .. Module.lspNumberOfInfo() or "",
+    "%=%-14.(󰉻 %l,%c%V%) %P"
+  })
+end
+
+function Module.get_default_line()
+  return table.concat({
+    vim.bo.filetype == "help" and "' " or " ",
+    "%f",
+    "%=%-14.(󰉻 %l,%c%V%) %P"
+  })
+end
+
+function Module.get_short_line()
+  local git_branch_name = Module.get_git_branch_name()
+  return table.concat({
+    " %f ",
+    git_branch_name ~= "" and " " .. git_branch_name or "",
+    "%=%-14.(󰉻 %l,%c%V%) %P"
+  })
 end
 
 return Module
--- plugin/init.lua
+++ plugin/init.lua
@@ -1,6 +1,16 @@
-vim.api.nvim_create_autocmd({"BufEnter", "CursorHold", "InsertLeave"}, {
+vim.api.nvim_create_autocmd({"FileType", "BufEnter", "CursorHold", "InsertLeave"}, {
   pattern = "*",
   callback = function(event)
-    require("nvim-statusline").set_statusline()
+    local statusline = require("nvim-statusline")
+    if not vim.bo.filetype or string.len(vim.bo.filetype) == 0 then
+      vim.opt_local.statusline = statusline.get_default_line()
+      return
+    end
+    if vim.bo.filetype == "netrw" or vim.bo.filetype == "NvimTree" then
+      vim.opt_local.statusline = statusline.get_short_line()
+      return
+    end
+
+    vim.opt_local.statusline = statusline.get_advanced_line()
   end
 })
